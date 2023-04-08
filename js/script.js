1ab88d7c9f97aab76931a24bf3a37b252206f907
René Delgado <cortex.r52@gmail.com>
2023-04-08 14:36:23 -0600

    Updating animation.

--- js/script.js
+++ js/script.js
@@ -9,23 +9,11 @@ function createFlash(x, y) {
     flash.classList.add('flash');
     flash.style.top = y + 'px';
     flash.style.left = x + 'px';
-    
-    // Agregar animaciones y efectos visuales
-    var colors = ['#FFC700', '#E24D4D', '#5BBEFF', '#FF5B5B', '#FF9F1C', '#2EC4B6', '#F26DF9'];
-    var colorIndex = Math.floor(Math.random() * colors.length);
-    flash.style.backgroundColor = colors[colorIndex];
-    flash.style.borderRadius = '50%';
-    flash.style.width = '100px';
-    flash.style.height = '100px';
-    flash.style.opacity = '0.8';
-    flash.style.position = 'fixed';
-    flash.style.zIndex = '99999';
-    flash.style.animation = 'majesticFlash 0.8s ease-out forwards';
-
+    flash.style.backgroundColor = getRandomColor();
     document.body.appendChild(flash);
     setTimeout(function () {
         flash.remove();
-    }, 800);
+    }, 500);
 }
 
 window.onload = function () {
